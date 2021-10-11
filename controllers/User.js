17dd90e4f64b4716a28aacbfae1c298488544566
Pratik-sys <pithore.pratik@gmail.com>
2021-10-11 18:51:11 +0530

    Add Login route

--- controllers/User.js
+++ controllers/User.js
@@ -1,19 +1,33 @@
 const { User } = require("../models");
 
 module.exports = {
-    userAdd : (req, res) => {
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password : req.body.password,
-            mobile_no : req.body.mobile,
-            gender : req.body.gender,
-            dob : Date.now()
-        }).save().then(() => {
-            return res.status(201).json({"msg" : "user Added successfully"})
-        }).catch((err) =>{
-            return res.status(404).json({"msg" : "Error while adding user"})
-        })
-    },
-}
-    
+  userRegister: (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      dob: Date.now(),
+      mobile_no: req.body.mobile,
+      password: req.body.password,
+      address: req.body.address,
+      gender: req.body.gender,
+    })
+      .save()
+      .then(() => {
+        return res.status(201).json({ msg: "user Added successfully" });
+      })
+      .catch((err) => {
+        return res.status(404).json({ msg: "Error while adding user" });
+      });
+  },
+  userLogin: (req,res) => {
+    const user = new User();
+    if (
+      (req.body.email === user.name) &
+      (req.body.password === user.password)
+    ) {
+      return res.status(201).json({ msg: "User logged in sucessfully" });
+    } else {
+      return res.status(404).json({ msg: "Login with proper Credentials" });
+    }
+  },
+};
