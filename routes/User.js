5bccb0c1cba50b01800618d3b52153543941785d
Pratik-sys <pithore.pratik@gmail.com>
2021-10-11 18:51:11 +0530

    Add Login route

--- routes/User.js
+++ routes/User.js
@@ -4,5 +4,6 @@ const User = express.Router();
 const { UserController } = require("../controllers");
 
 User.post("/register", UserController.userAdd);
+User.post("/login", UserController.userLogin);
 
 module.exports = User;
