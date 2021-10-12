8507275f3ace3bc1ad2dcba0f9ec29308f9c6187
Pratik-sys <pithore.pratik@gmail.com>
2021-10-12 09:45:07 +0530

    Product CRUD

--- controllers/Product.js
+++ controllers/Product.js
@@ -28,4 +28,33 @@ module.exports = {
             return res.status(404).json({ msg: "Product not found" });
         });
     },
+    productUpdate: (req, res) =>{
+        Product.findByIdAndUpdate({_id: req.params.id},{$set:{
+            name:req.body.name,
+            price : req.body.price,
+            details : req.body.details
+        }}).then(()=>{
+            return res.status(200).json({"msg" : "product updated"});
+        }).catch((err) =>{
+            return res.status(404).json({"msg" : "Error while updating"});
+        });
+    },
+    productDelete : (req, res) =>{
+        Product.remove({_id : req.params.id}, (err) =>{
+            if(!err){
+                return res.status(200).json({"msg" : "product deleted"});
+            }else{
+                return res.status(404).json({"msg" : "Error while deleting product"});
+            }
+        });
+    },
+    productGetAll : (res) =>{
+        Product.find({},(err, product) =>{
+            if(!err){
+                return res.status(200).json(product);
+            }else{
+                return res.status(404).json({"msg" : "Error while fetching all products"})
+            }
+        });
+    }
 };
--- routes/Product.js
+++ routes/Product.js
@@ -6,5 +6,8 @@ const { validateProduct } = require("../middlewares/validation");
 
 Product.post("/", validateProduct, ProductController.productAdd);
 Product.get("/:id", ProductController.productGetOne);
+Product.put("/:id/update", ProductController.productUpdate);
+Product.delete("/:id/delete", ProductController.productDelete);
+Product.get("/getAll", ProductController.productGetAll);
 
 module.exports = Product;
