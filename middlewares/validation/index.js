f79ef702503a549f744c8b81c52b6ab31687c973
Pratik-sys <pithore.pratik@gmail.com>
2021-10-19 22:38:45 +0530

    User validation

--- middlewares/validation/index.js
+++ middlewares/validation/index.js
@@ -1,5 +1,7 @@
 const validateProduct = require("./ProductValidation");
+const validateRegisterInput = require("./UserValidation")
 
 module.exports = {
     validateProduct,
+    validateRegisterInput
 };
